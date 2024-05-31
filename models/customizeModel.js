08021fdcbb738db32a957cbb2eedf41a11ee0089
Safayet <hossainsafayet187@gmail.com>
2024-05-31 23:04:37 +0600

    customize model

--- models/customizeModel.js
+++ models/customizeModel.js
@@ -12,8 +12,43 @@ var userSchema = new mongoose.Schema({
     date:{
         type:Date,
         required:true,
+    },
+    contact : {
+        firstName : {
+            type : String,
+            required:true, 
+        },
+        lastName : {
+            type : String,
+            required : true
+        },
+        email : {
+            type : String,
+            required : true
+        },
+        phone : {
+            type : String,
+            required : true
+        }
+    },
+    address : {
+        house : {
+            type : String,
+            required : true
+        },
+        road : {
+            type : String,
+            required : true
+        },
+        city : {
+            type : String,
+            required : true
+        },
+        postCode : {
+            type : String,
+            required : true
+        }
     }
 });
 
-//Export the model
 module.exports = mongoose.model('Customize', userSchema);
