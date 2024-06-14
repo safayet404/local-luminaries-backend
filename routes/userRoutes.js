6ab760f2e72ade2815473092579d8d8c34b7f7b4
Safayet <hossainsafayet187@gmail.com>
2024-06-15 00:46:48 +0600

    login controller added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { createUser, getAllUsers, getSingleUser, updateSingleUser, deleteSingleUser } = require('../controller/userController')
+const { createUser, getAllUsers, getSingleUser, updateSingleUser, deleteSingleUser, loginUser } = require('../controller/userController')
 const router = express.Router()
 
 router.post('/register',createUser)
+router.post("/login-user",loginUser)
 router.get('/all-user',getAllUsers)
 router.get('/single-user/:id',getSingleUser)
 router.put('/update-user/:id',updateSingleUser)
