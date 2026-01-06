49a254ca3ee52d583c483d2e0ec99b6a5b02d28d
Safayet Hossain <75946552+safayet404@users.noreply.github.com>
2026-01-06 22:18:13 +0600

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -8,4 +8,7 @@ router.get("/all-contact",getAllContact)
 router.get("/single-contact/:id",getSingleContact)
 router.put("/update-contact/:id",updateContact)
 router.delete("/delete-contact/:id",deleteContact)
+
+router.delete("/delete-contact/:id",deleteContact)
+
 module.exports = router
